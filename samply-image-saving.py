ff95ebfb495039f392c5099983967a629824693c
SSROutside <ssroutside@gmail.com>
2017-06-21 11:32:59 -0400

    Camera code works

--- samply-image-saving.py
+++ samply-image-saving.py
@@ -15,23 +15,24 @@ from scipy.ndimage import morphology as scmorph
 
 count = 0
 
-def main():
+with pyrs.Service() as a, pyrs.Device() as py_dev:
     # Start realsense (if available)
-    
-    pyrs.start()
+    py_dev.apply_ivcam_preset(0)
+
     
     # Set up device for colour and depth streaming.
     # How might you add a raw IR stream?
-    #py_dev = pyrs.Device(device_id = 0, streams = [pyrs.ColourStream(fps = 30), pyrs.DepthStream(fps=30)])
 
     # Jesus's changes:
 
     check = True
+
     while check  == True:
          print(" ") 
          folder_name = raw_input("What folder would you like to save these images in? ")
          if os.path.isdir(folder_name) == True: 
             check = False 
+
          if os.path.isdir(folder_name) == False:
             print(" ")
             print("That folder name does not exist. A new folder will be created in the current directory under that name. ")
@@ -41,8 +42,12 @@ def main():
                 os.makedirs(final_directory)
             check = False
             # for testing purposes, you can enter this: /Users/Jesus/Documents/REU 2017/misc-files
+
+
     check2 = True
     check3 = True
+    series_name = 'start'
+
     while check2 == True:
         print(" ")
         series = raw_input("Would you like to save related images in a series? Images stored in this series will have the same file name with a counting number at the end. [y/n]   ")       
@@ -54,94 +59,89 @@ def main():
                     print(" ")
                     print("A file already exists with that name. ")
                     check3 = True              
+
                 if os.path.isfile(series_name) == False:
                     print(" ")
                     print("That name will do I suppose...")
                     check3 = False
                 check2 = False
+
         else:
             print(" ")
             print("Well okay then, have fun thinking up a unique name for each picture... ")
             check2 = False
 
         
-baseline_name = series_name
-baseline_name = baseline_name + '_1.jpg'
-
-
-
-# to get date and time: datetime.date.today()
-
-
-            
+	baseline_name = series_name
+	baseline_name = baseline_name + '_1.jpg'
+	# to get date and time: datetime.date.today()            
     
-    # Run forever! (until quitting)
-    while True:
+    	# Run forever! (until quitting)
+	while True:
         
-        py_dev.wait_for_frame()
-        c_im = py_dev.colour
+        	py_dev.wait_for_frame()
+        	c_im = py_dev.colour
         
-        # Convert colour frame into a single RGB image
-        rgb_im = c_im[..., ::-1] #y tho
+        	# Convert colour frame into a single RGB image
+	        rgb_im = c_im[..., ::-1] #y tho
         
-        # scale depth frames to avoid colour wrapping. Is this the best scaling factor for our purposes? Try changing it
-        d_im = py_dev.depth * 0.05 #what units this be in
+      	  	# scale depth frames to avoid colour wrapping. Is this the best scaling factor for our purposes? Try changing it
+        	d_im = py_dev.depth * 0.05 #what units this be in
 
-        # Convert the 3D depth frame to a 2D colour map:
-        d_im_col = cv2.applyColorMap(d_im.astype(np.uint8), cv2.COLORMAP_HSV)
-        # what other colourmaps exist? are some more intuitive than others? Any that would be a problem for colourblind people?
-        # good ones to use (because they have a lot of color bins):
-        # COLORMAP_HSV
-        # COLORMAP_JET
-        # COLORMAP_RAINBOW
+        	# Convert the 3D depth frame to a 2D colour map:
+        	d_im_col = cv2.applyColorMap(d_im.astype(np.uint8), cv2.COLORMAP_HSV)
+	        # what other colourmaps exist? are some more intuitive than others? Any that would be a problem for colourblind people?
+	        # good ones to use (because they have a lot of color bins):
+	        # COLORMAP_HSV
+	        # COLORMAP_JET
+	        # COLORMAP_RAINBOW
 
         
-        # combine the two images into one array, for display
-        cd = np.concatenate((rgb_im, d_im_col), axis=1)
-        # axis 1 is side by side view
-        # axis 0 is one on top of other view
+        	# combine the two images into one array, for display
+        	cd = np.concatenate((rgb_im, d_im_col), axis=1)
+        	# axis 1 is side by side view
+        	# axis 0 is one on top of other view
         
-        # show two images in one frame (default name)
-        # To-do: what if we want to display multiple frames? look at the OpenCV imshow documentation for tips
-        cv2.imshow('', cd)
+        	# show two images in one frame (default name)
+        	# To-do: what if we want to display multiple frames? look at the OpenCV imshow documentation for tips
+        	cv2.imshow('', cd)
 
-        # To-do: how about displaying a point-cloud using VTK, in a different window?
-        # Visiting vtk info another day
+        	# To-do: how about displaying a point-cloud using VTK, in a different window?
+        	# Visiting vtk info another day
         
-        input = cv2.waitKey(1):
-        # quit if we hit 'q'
-        if (input == ord('q')):
-            break
-        elif (input == ord('a')):
-            os.chdir(final_directory)
+        	input = cv2.waitKey(1)
+        	# quit if we hit 'q'
+        	if (input == ord('q')):
+            		break
+        	elif (input == ord('a')):
+			print('got here')
+            		os.chdir(final_directory)
             
-            counter++;
-            # save depth and colour images if we hit 'a'.
-            # Note that there are many, many ways to save images in Python.
-            # OpenCV has one method in its library, but for faster image saving we could look into something like PIL.
-            baseline_name = baseline_name + '_'+counter+'.jpg' #keeps count of the photo taken in this particular series
-            cv2.imwrite(baseline_name + '_'+ counter +'_'+ str(datetime.date.today())+'_'+ time.strftime("%H:%M:%S")+'_'+ '_depth.PNG', d_im_col);
-            cv2.imwrite(baseline_name +'_'+ counter +'_'+ str(datetime.date.today()) +'_'+ time.strftime("%H:%M:%S") +'_'+ '_color.PNG', rgb_im);
-            # to-do: how do we save images to a specific folder? How can we let the user choose this folder on start up?
-            # the above imwrite commands have the format of series_count_date_time.png
+            		counter = counter + 1
+            		# save depth and colour images if we hit 'a'.
+            		# Note that there are many, many ways to save images in Python.
+            		# OpenCV has one method in its library, but for faster image saving we could look into something like PIL.
+            		baseline_name = baseline_name + '_'+counter  #keeps count of the photo taken in this particular series
+            		cv2.imwrite(baseline_name +'_'+ str(datetime.date.today())+'_'+ time.strftime("%H:%M:%S")+'_'+ '_depth.PNG', d_im_col);
+            		cv2.imwrite(baseline_name +'_'+ str(datetime.date.today()) +'_'+ time.strftime("%H:%M:%S") +'_'+ '_color.PNG', rgb_im);
+            		# to-do: how do we save images to a specific folder? How can we let the user choose this folder on start up?
+            		# the above imwrite commands have the format of series_count_date_time.png
 
-            # Jesus: I think we can add a raw_input line at the beginning of the code asking the user for a folder. We would also need to make sure that the inputed folder name actually exists. If input does not match an existing folder, would keep asking user for a folder name.
+            		# Jesus: I think we can add a raw_input line at the beginning of the code asking the user for a folder. We would also need to make sure that the inputed folder name actually exists. If input does not match an existing folder, would keep asking user for a folder name.
 
-            # Jesus: check this website out for explanation of imwrite:
-            # http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imwrite
+            		# Jesus: check this website out for explanation of imwrite:
+            		# http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imwrite
             
-            # Can we add an incrementing number to the filename of colour/depth?
+            		# Can we add an incrementing number to the filename of colour/depth?
 
-            # Jesus: add a counter in a for loop that sets the name of the file
+            		# Jesus: add a counter in a for loop that sets the name of the file
             
-            # What about having the user type a new filename every time we save?
+            		# What about having the user type a new filename every time we save?
 
-            # Jesus: I like this idea. Gives the user a bit more freedom. Downside is that we don't know if its tree 6 or tree 7. Checking to see if the filename already exists would solve that problem.
+            		# Jesus: I like this idea. Gives the user a bit more freedom. Downside is that we don't know if its tree 6 or tree 7. Checking to see if the filename already exists would solve that problem.
             
-        elif (input == -1):
-            continue
+        	elif (input == -1):
+            		continue
 
 
 
-if __name__ == '__main__':
-    main()
